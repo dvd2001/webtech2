f1e903cff44e758ed92d7dc75d36f222a9971a94
dvd2001 <bedavid2001@gmail.com>
2026-04-13 14:12:56 +0200

    setting up app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
-export const routes: Routes = [];
+import { DashboardComponent } from './components/dashboard-component/dashboard.component';
+import { LoginComponent } from './components/login-component/login.component';
+import { RegistrationComponent } from './components/registration-component/registration.component';
+import { ProductCreateComponent } from './components/product-create-component/product-create.component';
+import { ProductListComponent } from './components/product-list-component/product-list.component';
+import { ProductEditComponent } from './components/product-edit-component/product-edit.component';
+
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'login' },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegistrationComponent },
+    { path: 'products/create', component: ProductCreateComponent },
+    { path: 'products', component: ProductListComponent },
+    { path: 'products/:id/edit', component: ProductEditComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
