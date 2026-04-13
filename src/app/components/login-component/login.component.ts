dd6929c9b5158be6b9f5259d60989e09f596837b
dvd2001 <bedavid2001@gmail.com>
2026-04-13 11:47:45 +0200

    creating the base of the login component

--- src/app/components/login-component/login.component.ts
+++ src/app/components/login-component/login.component.ts
@@ -1,9 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { UserService } from '../../services/user-service/user.service';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-login.component',
-  imports: [],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+  loginForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl('')
+  });
+  users: any = [];
+
+  constructor(private router: Router, private userService: UserService) { }
+
+  ngOnInit(): void { }
+
+  get datas() { return this.loginForm.value; }
+
+  onsubmit() {
+    let currentUser = (JSON.stringify(this.loginForm.value?.username ?? ''));
+    let currentPassword = (JSON.stringify(this.loginForm.value?.password ?? ''));
+
+    this.userService.getUsers().subscribe((data) => {
+      this.users = data;
+      for (let user of this.users) {
+        if (currentUser === JSON.stringify(user.username) && currentPassword === JSON.stringify(user.password)) {
+          this.userService.setCurrentUser(user);
+          this.router.navigate(['/products']);
+          return;
+        }
+      }
+      const errorElement = document.getElementById('wrongDatas');
+      if (errorElement) {
+        errorElement.innerHTML = 'Wrong username or password!';
+      }
+    });
+  }
+}
