23e3f860ea4ec11658d11f9393d7499adc34ac38
dvd2001 <bedavid2001@gmail.com>
2026-04-13 11:53:22 +0200

    creating product service

--- src/app/services/product-service/product.service.ts
+++ src/app/services/product-service/product.service.ts
@@ -1,6 +1,56 @@
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Product } from '../../models/product';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ProductService {}
+export class ProductService {
+
+  private baseUrl = 'http://localhost:4000/api/products';
+  private headers = new HttpHeaders().set('Content-Type', 'application/json');
+  constructor(private http: HttpClient) { }
+
+  createProduct(data: any): Observable<any> {
+    return this.http.post(this.baseUrl, data).pipe(catchError(this.errorMgmt));
+  }
+
+  getProducts(): Observable<any> {
+    return this.http.get(this.baseUrl);
+  }
+
+  getProduct(id: any): Observable<any> {
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.get(url, { headers: this.headers }).pipe(
+      map((res: any) => {
+        return res || {};
+      }),
+      catchError(this.errorMgmt)
+    );
+  }
+
+  updateProduct(id: any, data: any): Observable<any> {
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.put(url, data, { headers: this.headers }).pipe(catchError(this.errorMgmt));
+  }
+
+  deleteProduct(id: any): Observable<any> {
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.delete(url, { headers: this.headers }).pipe(catchError(this.errorMgmt));
+  }
+
+  errorMgmt(error: HttpErrorResponse) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  }
+}
