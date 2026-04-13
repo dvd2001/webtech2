be5a25fd49a9e66bfab12d6e69cb68572aaf6742
dvd2001 <bedavid2001@gmail.com>
2026-04-13 14:54:51 +0200

    creating dashboard base

--- src/app/components/dashboard-component/dashboard.component.ts
+++ src/app/components/dashboard-component/dashboard.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user-service/user.service';
+import { User } from '../../models/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard.component',
-  imports: [],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {}
+export class DashboardComponent implements OnInit {
+  user = new User();
+  username!: string;
+  name!: string;
+
+  constructor(private userService: UserService, private router: Router) { this.getUser(); }
+
+  ngOnInit(): void { }
+  getUser(): void {
+    if (this.userService.getCurrentUser().username == null) {
+      this.router.navigate(['/login']);
+    }
+    this.user = this.userService.getCurrentUser();
+    this.name = JSON.stringify(this.user.name);
+    this.username = JSON.stringify(this.user.username);
+  }
+
+  logout(): void {
+    this.user = new User();
+    this.userService.setCurrentUser(this.user);
+    this.router.navigate(['/login']);
+  }
+}
