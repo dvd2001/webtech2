1fab3be3ec829bfd34bf42ec2087c8a26e2b31d5
dvd2001 <bedavid2001@gmail.com>
2026-04-13 16:00:59 +0200

    creating product list base

--- src/app/components/product-list-component/product-list.component.ts
+++ src/app/components/product-list-component/product-list.component.ts
@@ -1,9 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../models/product';
+import { ProductService } from '../../services/product-service/product.service';
+import { UserService } from '../../services/user-service/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-list.component',
-  imports: [],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css',
 })
-export class ProductListComponent {}
+export class ProductListComponent implements OnInit {
+  Products: Product[] = [];
+  constructor(private router: Router, private productService: ProductService, private userService: UserService) {
+    this.readProducts();
+    this.getUser();
+  }
+  ngOnInit(): void { }
+  readProducts() {
+    this.productService.getProducts().subscribe((data) => {
+      this.Products = data;
+    });
+  }
+  getUser() {
+    if (this.userService.getCurrentUser() === null) {
+      this.router.navigate(['/login']);
+    }
+  }
+
+  edit(index: number) {
+    const id = this.Products[index]._id;
+    this.router.navigate(['/products/' + id + '/edit']);
+  }
+
+  delete(index: number) {
+    const id = this.Products[index]._id;
+    this.productService.deleteProduct(id).subscribe(() => {
+      this.readProducts();
+    });
+  }
+}
