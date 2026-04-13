4bc693b11d01b310fccc00934e32fca9cf789da5
dvd2001 <bedavid2001@gmail.com>
2026-04-13 15:36:37 +0200

    implementing function for product creation

--- src/app/components/product-create-component/product-create.component.ts
+++ src/app/components/product-create-component/product-create.component.ts
@@ -1,8 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { Router } from '@angular/router';
+import { ProductService } from '../../services/product-service/product.service';
+import { UserService } from '../../services/user-service/user.service';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-product-create.component',
   templateUrl: './product-create.component.html',
   styleUrl: './product-create.component.css',
 })
-export class ProductCreateComponent { }
+export class ProductCreateComponent implements OnInit {
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private productService: ProductService,
+    private userService: UserService,
+    private router: Router,
+    private ngZone: NgZone
+  ) {
+    this.mainForm();
+    this.getUser();
+  }
+
+  get myFrom() {
+    return this.createProductForm.controls;
+  }
+
+  submitted = false;
+  createProductForm!: FormGroup;
+  matcher = new MyErrorStateMatcher();
+
+  ngOnInit(): void { }
+
+  mainForm() {
+    this.createProductForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(5), Validators.pattern('^[a-zA-Z]+$')]],
+      quantity: ['', [Validators.required, Validators.min(0), Validators.pattern('^[0-9]+$')]],
+      typeOfQuantity: ['', [Validators.required]],
+    });
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+    if (!this.createProductForm.valid) {
+      return;
+    } else {
+      this.productService.createProduct(this.createProductForm.value).subscribe(
+        (res) => {
+          console.log('Product successfully created!');
+          this.ngZone.run(() => this.router.navigateByUrl('/products'));
+        }, (error) => { console.log(error); }
+      );
+    }
+  }
+
+  getUser() {
+    if (this.userService.getCurrentUser() === null) {
+      this.router.navigateByUrl('/login');
+    }
+  }
+}
